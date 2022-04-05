74c7baf121ece0d05ef7dd8c4bf02e071cf4c15f
Baha <bmanas1@gmail.com>
2022-04-05 10:11:58 -0400

    v1

--- src/deneme1/deneme1.java
+++ src/deneme1/deneme1.java
@@ -3,5 +3,6 @@ package deneme1;
 public class deneme1 {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Intelliji de bir ok tan bahsedildi");
     }
 }
